092ef6ad9d45feb321aa08e86573e8a2c42fff40
Jeremy <jeremyjong@gmail.com>
2022-07-01 22:35:04 +0900

    final commit
    Signed-off-by: Jeremy <jeremyjong@gmail.com>

---
+++ AutoFolderImages/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoFolderImages
+{
+    class Program
+    {    
+
+        public static readonly List<string> ImageExtensions = new List<string> { ".JPG", ".JPEG", ".BMP", ".GIF", ".PNG", ".WEBM" };
+
+
+        static void Main(string[] args)
+        {
+
+           
+
+            DateTime today = DateTime.Today;
+            var folder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            var files = Directory.GetFiles(folder);
+
+            string destinationDirectory = "D:\\Desktop\\" + DateTime.Today.ToString("yyyyMMdd");
+
+
+            foreach (var f in files)
+            {
+                if (ImageExtensions.Contains(Path.GetExtension(f).ToUpperInvariant()))
+                {
+                    System.IO.Directory.CreateDirectory(destinationDirectory);                
+                    System.IO.File.Move(f, destinationDirectory + "\\" + Path.GetFileName(f));
+                }
+            }
+
+
+        }
+
+
+    }
+}
